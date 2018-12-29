11a1cd0a138e825744f5ce612d6c06fed2f0b6ad
Jethro Whaley <jethro.whaley@gmail.com>
2018-12-29 20:42:10 +0100

    testing for new message not saving

--- spec/features/new_message_spec.rb
+++ spec/features/new_message_spec.rb
@@ -1,4 +1,11 @@
 feature 'new message' do
+  scenario 'empty message does not save' do
+    Timecop.freeze
+    visit('/')
+    click_button('submit')
+    expect(page).not_to have_content(Time.now)
+  end
+
   scenario 'send new message and display it' do
     submit_a_message
     expect(page).to have_content('This is a test mess')
